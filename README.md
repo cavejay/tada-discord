b898e3d7bc2a2152ad26862e6aa9730cf657c2c9
cavejay <michael@sablecliff.com>
2018-10-13 11:43:26 +1100

    bot now joins channels

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Discord Bot for dramatic entrances
 - [x] Action when user joins a voice channel
 - [ ] Play a 'noise' when user joins a voice channel
 - [ ] Play a user specific noise when a user joins vc
-- [ ]
+- [ ] Persistent Backend for Configuration
+- [ ] Entry/exit time-out to prevent spam
+- [ ] Collision avoidance if more than one user joins
+- [ ] Storage of user specified sound-bytes
--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const Discord = require("discord.js"); // Import the discord.js module
 const client = new Discord.Client(); // Create an instance of a Discord client
 const auth = require("./auth.json");
 
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
+const meID = "181615330861252608";
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
  */
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("We're online!");
 });
 
 // Create an event listener for messages
@@ -23,25 +21,58 @@ client.on("message", message => {
   }
 });
 
-client.on("voiceStateUpdate", (oldMember, newMember) => {
+client.on("error", e => {
+	console.log('client error:', e)
+});
+
+
+client.on("voiceStateUpdate", async (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel;
   let oldUserChannel = oldMember.voiceChannel;
 
   if (oldUserChannel === undefined && newUserChannel !== undefined) {
-    let msg = `${newUserChannel.displayName} joined channel ${
-      newUserChannel.name
-    }`;
+    let msg = `${newMember.displayName} joined channel ${newUserChannel.name}`;
 
     // User Joins a voice channel
     console.log(msg);
-    newMember
+    client.users.get(meID)
       .send(msg)
       .then(message => console.log(`Sent message: ${message.content}`))
       .catch(console.error);
+
+    // Quick! Play their entry noise:
+    if (newUserChannel) {
+      console.log("apparently we never reach this?");
+      let connection = await newUserChannel.join()
+ 
+          console.log(
+            `Bot has joined channel ${newUserChannel.name} to 'tada' someone`
+          );
+
+          // create the dispatcher to play the tada noise
+          const dispatcher = connection.playFile(
+            "/home/cj/proj/tada-discord/none.mp3"
+          );
+
+          // when it ends let us know
+          dispatcher.on("end", () => {
+            // log finishing
+            console.log(
+              `Bot has finished 'tadaing' ${newMember.displayName} in channel ${
+                newUserChannel.name
+              }`
+            );
+
+            // leave the channel
+            newUserChannel.leave()
+          });
+
+          dispatcher.on("error", err => {
+            console.log('dispatcher error:', err);
+          });
+    }
   } else if (newUserChannel === undefined) {
-    let msg = `${oldUserChannel.displayName} left channel ${
-      oldUserChannel.name
-    }`;
+    let msg = `${oldMember.displayName} left channel ${oldUserChannel.name}`;
 
     // User leaves a voice channel
     console.log(msg);
