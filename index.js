5c4518f49db1a939bbb7cf8031c484a00375ae75
cavejay <michael@sablecliff.com>
2018-10-14 09:14:52 +1100

    well.. it works now

--- index.js
+++ index.js
@@ -12,15 +12,7 @@ client.on("ready", () => {
   console.log("We're online!");
 });
 
-// Create an event listener for messages
-client.on("message", message => {
-  // If the message is "ping"
-  if (message.content === "ping") {
-    // Send "pong" to the same channel
-    message.channel.send("pong");
-  }
-});
-
+// let us know about errors
 client.on("error", e => {
 	console.log('client error:', e)
 });
@@ -30,8 +22,17 @@ client.on("voiceStateUpdate", async (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel;
   let oldUserChannel = oldMember.voiceChannel;
 
+  // IF the user is joining a channel
   if (oldUserChannel === undefined && newUserChannel !== undefined) {
     let msg = `${newMember.displayName} joined channel ${newUserChannel.name}`;
+
+    // If it's me I don't care. plz check
+    if (newMember.id === client.user.id) {
+      console.log(msg)
+      return
+    }
+
+    // Make sure I have the correct permissions to access the channel!
 
     // User Joins a voice channel
     console.log(msg);
@@ -42,34 +43,33 @@ client.on("voiceStateUpdate", async (oldMember, newMember) => {
 
     // Quick! Play their entry noise:
     if (newUserChannel) {
-      console.log("apparently we never reach this?");
       let connection = await newUserChannel.join()
  
-          console.log(
-            `Bot has joined channel ${newUserChannel.name} to 'tada' someone`
-          );
+      console.log(
+        `Bot has joined channel ${newUserChannel.name} to 'tada' someone`
+      );
 
-          // create the dispatcher to play the tada noise
-          const dispatcher = connection.playFile(
-            "/home/cj/proj/tada-discord/none.mp3"
-          );
+      // create the dispatcher to play the tada noise
+      const dispatcher = await connection.playFile(
+        "./tada.mp3"
+      );
 
-          // when it ends let us know
-          dispatcher.on("end", () => {
-            // log finishing
-            console.log(
-              `Bot has finished 'tadaing' ${newMember.displayName} in channel ${
-                newUserChannel.name
-              }`
-            );
+      // when it ends let us know
+      dispatcher.on("end", () => {
+        // log finishing
+        console.log(
+          `Bot has finished 'tadaing' ${newMember.displayName} in channel ${
+            newUserChannel.name
+          }`
+        );
 
-            // leave the channel
-            newUserChannel.leave()
-          });
+        // leave the channel
+        newUserChannel.leave()
+      });
 
-          dispatcher.on("error", err => {
-            console.log('dispatcher error:', err);
-          });
+      dispatcher.on("error", err => {
+        console.log('dispatcher error:', err);
+      });
     }
   } else if (newUserChannel === undefined) {
     let msg = `${oldMember.displayName} left channel ${oldUserChannel.name}`;
