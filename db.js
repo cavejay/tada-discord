ae9b3e505051540cdefd268f94c801887726b3f0
cavejay <michael@sablecliff.com>
2018-10-15 23:55:59 +1100

    more db stuff

--- db.js
+++ db.js
@@ -9,10 +9,25 @@ db.raw = new Datastore({
   autoload: true
 });
 
-db.userprefs = async function() {
-  p.info(`Creating a new document for video: `);
-  // Should we do some checks first?
-  const res = await db.raw.insert({});
+db.makeUser = async function(user) {
+  p.info(`Making new user ${user}`);
+  const res = await db.raw.insert({userID: user, intro: "tada"});
+  p.info(res)
+}
+
+db.getUserIntro = async function (user) {
+  p.info(`Finding intro listed for ${user}`)
+  const res = await db.getUser(user)
+  return res.intro
+}
+
+db.getUser = async function(user) {
+  p.info(`Attempting to fetch user: ${user}`);
+  
+  const res = await db.raw.findOne({userID: user});
+
+  // this isn't returning undefined :'(
+  p.info(res)
   return res;
 };
 
