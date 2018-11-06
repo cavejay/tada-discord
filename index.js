911c8fa93a81ef62a6fc7b5f0af922a2c8f5edd8
cavejay <michael@sablecliff.com>
2018-11-07 00:12:02 +1100

    soundManager will replace sounds.config.json with dynamic management

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const config = require("./config.json");
 const db = require('./lib/db')
 const p = require("./lib/loggerFactory")("index");
 
-const Bot = require('./lib/bot')(db, config);
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-Bot.login(config.auth.bot.token);
+async function Main () {
+    const soundManager = await require('./lib/soundManager')({db,config})
+    const Bot = require('./lib/bot')({db, config, soundManager});
+
+    // Log our bot in using the token from https://discordapp.com/developers/applications/me
+    Bot.login(config.auth.bot.token);
+}
+
+try {
+    Main()
+} catch (err) {
+    p.error(err)
+}
