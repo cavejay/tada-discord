f663008c98444415bc46a78d34794912bf7c6203
cavejay <michael@sablecliff.com>
2019-01-04 21:07:11 +1100

    simple handling of 'send tada intros' functionality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const config = require("./config.json");
 
 const db = require("./lib/db");
+const soundManager = require("./lib/soundManager");
 const p = require("./lib/loggerFactory")("index");
 
 async function Main() {
-  const soundManager = await require("./lib/soundManager")({ db, config });
+  soundManager.init({ db, config });
   const Bot = require("./lib/bot/bot.main.js")({ db, config, soundManager });
 
   // Log our bot in using the token from https://discordapp.com/developers/applications/me
