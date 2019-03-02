d72e5627a959efa4c14965555b5e09c11d10d1ac
cavejay <michael@sablecliff.com>
2019-03-03 07:24:39 +1100

    working commando integration :D

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const config = require("./config.json");
+const p = require("./lib/loggerFactory")("index");
 
 const db = require("./lib/db");
 const soundManager = require("./lib/soundManager");
-const p = require("./lib/loggerFactory")("index");
+const AddCommands = require("./lib/addCommands");
 
 async function Main() {
   soundManager.init({ db, config });
   const Bot = require("./lib/bot/bot.main.js")({ db, config, soundManager });
+
+  // Add Commands to Bot
+  AddCommands(Bot);
 
   // Log our bot in using the token from https://discordapp.com/developers/applications/me
   Bot.login(config.auth.bot.token);
