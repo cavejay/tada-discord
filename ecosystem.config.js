626d94b7eebeb99996bccb97292531247a81dba1
cavejay <michael@sablecliff.com>
2019-04-29 22:09:05 +1000

    adding pm2 run file

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'Tada-Stable',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {}
+  }],
+};
