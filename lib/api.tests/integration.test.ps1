62676a25ec1806331c2792ca4d18675618ded3cb
cj <cj@sablecliff.com>
2020-06-11 22:52:03 +1000

    added more API info Started writing API tests

--- lib/api.tests/integration.test.ps1
+++ lib/api.tests/integration.test.ps1
@@ -38,38 +38,115 @@ function get-jsDate {
   return [Math]::Floor(1000 * (Get-Date ([datetime]::UtcNow) -UFormat %s))
 }
 
-# Describe 'Simple' {
-#   It "Qer welcome message on /api is present" {
-#     $res = Invoke-WebRequest -UseBasicParsing -method GET -Uri "$base/api"
-#     $res.content | Should -BeExactly "Welcome to Qer, the simple HTTP based queue service"
-#     $res.StatusCode | Should -BeExactly 200
-#   }
-
-#   $_channel = "testChannel_$( get-jsDate )"
-#   $_uri = "$base/api/$_channel"
-#   $b = "{`"foo`": `"bar`"}"
-
-#   It "Channel creation provides expected response" {
-#     Write-Host "POSTing to $_uri with $b"
-        
-#     $res = Invoke-WebRequest -UseBasicParsing -method POST -Uri $_uri -Body $b  -ContentType "application/json"
-#     $res.content | Should -BeExactly $stdResponses["POST_201Created"]
-#     $res.StatusCode | Should -BeExactly 201
-#   }
-
-# }
-
 # $uri = "$base/api/deleteeverything"
 # write-host -ForegroundColor cyan -Object "GET $uri"
 # $res = Invoke-WebRequest -Method GET -Uri "$uri"
 # write-host $res
 
-$uri = "$base/api/user/asdlfkjasldkfj/guild/1231231213"
-write-host -ForegroundColor cyan -Object "POST $uri"
-$res = Invoke-WebRequest -Method POST -Uri "$uri"
-write-host $res
+function webget ($method, $uri, $body) {
+  write-host -ForegroundColor cyan -Object "$method $uri"
+  $res = $null
+  try {
+    if ($body) {
+      $res = Invoke-WebRequest -Method $method -Uri $uri -body $body -ErrorAction stop
+    } else {
+      $res = Invoke-WebRequest -Method $method -Uri $uri -ErrorAction stop
+    }
+  }
+  catch {
+    $res = $_.Exception.Response 
+  }
+  write-host $res.content
+  return $res
+}
 
-write-host -ForegroundColor cyan -Object "GET $uri"
-$res = Invoke-WebRequest -Method GET -Uri "$uri"
-write-host $res
+BeforeAll {
+  write-host -ForegroundColor RED "Removing test's user and guild setups"
+  $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+  $res = webget DELETE $uri
 
+  $uri = "$base/api/guild/411102766929543178"
+  $res = webget DELETE $uri
+
+  $uri = "$base/api/intro"
+  $res = webget POST $uri "{data: '192301923lkjlasdulkasjdlkasjd', filename: 'tada', id: 1}"
+
+  $uri = "$base/api/guild/411102766929543178"
+  $res = webget POST $uri "{}"
+
+}
+
+Describe 'New User joining a channel' {
+  It "attempts to fetch a non-existent user from API" {
+    $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+    $res = webget GET $uri
+
+    $res.StatusCode | Should -BeExactly 404
+  }
+
+  $introid = $null
+  It "creates a new user for the person that joined the channel" {
+    $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+    $res = webget -Method POST -Uri "$uri"
+
+    $res.content | Should -BeLike "{*}"
+    $introid = $res.content | ConvertFrom-Json | Select-Object -Property introID
+
+    $res.StatusCode | Should -BeExactly 201
+  }
+
+  It "fetches the user's intro" {
+    $uri = "$base/api/intro/$introid"
+    $res = webget -method GET -uri $uri
+
+    $res.StatusCode | Should -BeExactly 200
+  }
+}
+
+Describe 'Returning User joins a channel' {
+  $introid = $null
+  It "attempts to fetch an existing user from the API" {
+    $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+    $res = webget GET $uri
+
+    $res.StatusCode | Should -BeExactly 200
+
+    $introid = ($res.content | ConvertFrom-Json)[0].introid
+  }
+
+  It "fetches the user's intro" {
+    $uri = "$base/api/intro/$introid"
+    $res = webget GET $uri
+
+    $res.StatusCode | Should -BeExactly 200
+  }
+}
+
+Describe "User updating their intro" {
+  $introid = $null
+  It "Check that intro exists in guild" {
+    $uri = "$base/api/guild/411102766929543178"
+    $res = webget GET $uri
+
+    $res.StatusCode | Should -BeExactly 200
+  }
+
+  $resdata = $null
+  It "Retrieve the user's current guild data" {
+    $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+    $res = webget GET $uri
+    $resdata = $res.content
+    $res.StatusCode | Should -BeExactly 200
+  }
+
+  It "Update user's guild data to use the new introid" {
+    $uri = "$base/api/user/181615330861252608/guild/411102766929543178"
+    $userdata = ConvertFrom-Json $resdata
+    $userdata.introid = $introid
+    $body = ConvertTo-Json $userdata -Compress -Depth 10
+
+    $res = webget PUT $uri $body
+
+    $res.StatusCode | Should -BeExactly 200
+  }
+}
