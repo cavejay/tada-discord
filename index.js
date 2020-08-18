045f151f56d2d0867da826d16635f7ea83e47533
cj <cj@sablecliff.com>
2020-08-18 15:00:27 +1000

    We now save and play from db!

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const cfg = require("./config." + env);
 
 const TadaBot = require("./lib/bot");
 
-const Api = require("./lib/api");
+const TadaDBConnection = require("./lib/api/database.main");
 
-const TBDatabase = require("./lib/api/database.main");
+const Api = require("./lib/api");
 
 async function Main() {
   // Initialise the API Component
   // let apiComponent = await Api({ cfg });
+
+  // db
+  global.db = new TadaDBConnection({ cfg });
+  await global.db.init();
 
   //
   const Bot = new TadaBot({ cfg });
